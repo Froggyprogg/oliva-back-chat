7360bde40e266f7debd4b987a1550de640c03d69
Froggyprogg <romasenko764@gmail.com>
2024-01-29 12:39:57 +0300

    Chat is working in terminal read README.md

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # oliva-back-chat
 Chat service for oliva-med
+
+# How to run
+
+go run cmd/app/main.go - create a server
+
+go run cmd/app/client.go - create a client (for chatting needs to run 2 times)
+
+All command should be done in different terminal tabs!!!
--- cmd/app/client.go
+++ cmd/app/client.go
@@ -5,32 +5,33 @@ import (
 	"google.golang.org/grpc"
 	"log"
 	gchat "oliva-back-chat/internal/gen"
-	client2 "oliva-back-chat/pkg/handlers/chat"
+	client2 "oliva-back-chat/pkg/handlers/client"
 )
 
-func RunClient() {
-	const serverID = "localhost:5000"
-
-	log.Println("Connecting : " + serverID)
-	conn, err := grpc.Dial(serverID)
+func main() {
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
 
 	if err != nil {
-		log.Fatalf("Failed to connect gRPC server :: %v", err)
+		log.Fatalf("Faile to conncet to gRPC server :: %v", err)
 	}
 	defer conn.Close()
 
+	//call ChatService to create a stream
 	client := gchat.NewServicesClient(conn)
 
 	stream, err := client.ChatService(context.Background())
 	if err != nil {
-		log.Fatalf("Failed to get response from gRPC server :: %v", err)
+		log.Fatalf("Failed to call ChatService :: %v", err)
 	}
+
+	// implement communication with gRPC server
 	ch := client2.ClientHandle{Stream: stream}
 	ch.ClientConfig()
 	go ch.SendMessage()
 	go ch.ReceiveMessage()
 
-	// block main
+	//blocker
 	bl := make(chan bool)
 	<-bl
+
 }
